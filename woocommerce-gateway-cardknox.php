59ea08a66da7c8cf9ca5f86f0dfcb8dabb576388
dlehren <dovidleibe@gmail.com>
2018-01-18 09:39:49 -0500

    updated license

--- woocommerce-gateway-cardknox.php
+++ woocommerce-gateway-cardknox.php
@@ -15,35 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-/*
- * Plugin Name: WooCommerce Cardknox Gateway
- * Plugin URI: https://wordpress.org/plugins/woocommerce-gateway-cardknox/
- * Description: Take credit card payments on your store using Cardknox.
- * Author: WooCommerce
- * Author URI: https://woocommerce.com/
- * Version: 3.2.3
- * Requires at least: 4.4
- * Tested up to: 4.8
- * WC requires at least: 2.5
- * WC tested up to: 3.1
- * Text Domain: woocommerce-gateway-cardknox
- * Domain Path: /languages
- *
- * Copyright (c) 2017 WooCommerce
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;

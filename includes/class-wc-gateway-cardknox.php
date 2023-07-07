fc0709d5e079fc74ae56c3e5ca6ed26ef7c7c55c
Pritesh.Modi <pritesh.lr@gmail.com>
2023-07-07 20:27:10 +0530

    fix sonar cloud issue

--- includes/class-wc-gateway-cardknox.php
+++ includes/class-wc-gateway-cardknox.php
@@ -247,7 +247,7 @@ class WC_Gateway_Cardknox extends WC_Payment_Gateway_CC {
 	 * Initialise Gateway Settings Form Fields
 	 */
 	public function init_form_fields() {
-		$this->form_fields = require_once 'settings-cardknox.php';;
+		$this->form_fields = require_once 'settings-cardknox.php';
 	}
 	
 	/**
@@ -256,36 +256,32 @@ class WC_Gateway_Cardknox extends WC_Payment_Gateway_CC {
 	* @return bool
 	*/
 	public function is_valid_pay_for_order_endpoint() {
-
-		// If not on the pay for order page, return false.
-		if ( ! is_wc_endpoint_url( 'order-pay' ) || ! isset( $_GET['key'] ) ) {
-			return false;
-		}
-
-		$order_id = wc_get_order_id_by_order_key( wc_clean( wp_unslash( $_GET['key'] ) ) );
-
-		// If the order ID is not found or the order ID does not match the order ID in the URL, return false.
-		if ( ! $order_id || ( absint( get_query_var( 'order-pay' ) ) !== absint( $order_id ) ) ) {
-			return false;
-		}
-
-		$order = wc_get_order( $order_id );
-
-		// If the order doesn't need payment, we don't need to prepare the payment page.
-		if ( ! $order->needs_payment() ) {
-			return false;
-		}
-
-		// If it's not a guest order, current user can pay but only if it's their own order,
-		// or they are an admin or shop manager.
-		$order_has_customer = ! empty( $order->get_customer_id() );
-		if ( $order_has_customer ) {
-			$is_order_owned_by_current_user = $order->get_customer_id() === get_current_user_id();		
-			return $is_order_owned_by_current_user || current_user_can( 'manage_woocommerce' );
-		}
-
-		return true;
+		$is_valid = false;
+	
+		// If on the pay for order page and a valid key is set.
+		if ( is_wc_endpoint_url( 'order-pay' ) && isset( $_GET['key'] ) ) {
+			$orderid = wc_get_order_id_by_order_key( wc_clean( wp_unslash( $_GET['key'] ) ) );
+	
+			// If the order ID is found and matches the order ID in the URL.
+			if ( $orderid && absint( get_query_var( 'order-pay' ) ) === absint( $orderid ) ) {
+				$order = wc_get_order( $orderid );
+	
+				// If the order needs payment.
+				if ( $order->needs_payment() ) {
+					$order_customer = ! empty( $order->get_customer_id() );
+	
+					// If it's a guest order or the current user can manage WooCommerce.
+					if ( ! $order_customer || current_user_can( 'manage_woocommerce' ) ) {
+						$is_valid = true;
+					} elseif ( $order->get_customer_id() === get_current_user_id() ) {
+						$is_valid = true;
+					}
+				}
+			}
+		}	
+		return $is_valid;
 	}
+
 	/**
 	 * Payment form on checkout page
 	 */

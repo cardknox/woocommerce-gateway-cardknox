6e05685ded0a138a676e13d6a1bbcf4afdb0c0a0
pritesh.lr@gmail.com <pritesh.lr@gmail.com>
2023-12-12 04:55:56 -0500

    Fixed SonarCloud Quality

--- includes/settings-cardknox-googlepay.php
+++ includes/settings-cardknox-googlepay.php
@@ -7,81 +7,80 @@ global $woocommerce;
 $countries_obj   = new WC_Countries();
 $countries   = $countries_obj->__get('countries');
 
-$textType = 'text';
-$selectType = 'select';
+$gtextType = 'text';
+$gselectType = 'select';
 $googlePrefix = 'Google Pay';
-$keyPrefix = 'googlepay';
-$keyTitle = 'title';
-$keyType = 'type';
-$keyOptions = 'options';
-$keyDefault = 'default';
-$keyDescTip = 'desc_tip';
-$buttonPrefix = 'Google Pay Button';
+$gkeyPrefix = 'googlepay';
+$gkeyTitle = 'title';
+$gkeyType = 'type';
+$gkeyOptions = 'options';
+$gkeyDefault = 'default';
+$gkeyDescTip = 'desc_tip';
+$gbuttonPrefix = 'Google Pay Button';
 
 global $woocommerce;
 $countries_obj   = new WC_Countries();
 $countries   = $countries_obj->__get('countries');
 
 $wc_cardknox_google_pay_settings = array(
-    $keyPrefix . '_enabled' => array(
-        $keyTitle       => __('Enabled', 'woocommerce-gateway-cardknox'),
-        $keyType        => $selectType,
-        $keyOptions     => array(
+    $gkeyPrefix . '_enabled' => array(
+        $gkeyTitle       => __('Enabled', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => $gselectType,
+        $gkeyOptions     => array(
             'yes'    => __('Yes', 'woocommerce-gateway-cardknox'),
             'no'     => __('No', 'woocommerce-gateway-cardknox'),
         ),
     ),
-    $keyPrefix . '_title' => array(
-        $keyTitle       => __('Title', 'woocommerce-gateway-cardknox'),
-        $keyType        => $textType,
-        $keyDefault     => __('Google Pay', 'woocommerce-gateway-cardknox'),
+    $gkeyPrefix . '_title' => array(
+        $gkeyTitle       => __('Title', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => $gtextType,
+        $gkeyDefault     => __('Google Pay', 'woocommerce-gateway-cardknox'),
     ),
-    $keyPrefix . '_merchant_name' => array(
-        $keyTitle       => __('Merchant Name', 'woocommerce-gateway-cardknox'),
-        $keyType        => $textType,
-        $keyDefault     => __('Example Merchant', 'woocommerce-gateway-cardknox'),
+    $gkeyPrefix . '_merchant_name' => array(
+        $gkeyTitle       => __('Merchant Name', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => $gtextType,
+        $gkeyDefault     => __('Example Merchant', 'woocommerce-gateway-cardknox'),
     ),
-    $keyPrefix . '_environment' => array(
-        $keyTitle       => __('Environment', 'woocommerce-gateway-cardknox'),
-        $keyType        => $selectType,
-        $keyOptions     => array(
+    $gkeyPrefix . '_environment' => array(
+        $gkeyTitle       => __('Environment', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => $gselectType,
+        $gkeyOptions     => array(
             'TEST'          => __('Test', 'woocommerce-gateway-cardknox'),
             'PRODUCTION'    => __('Production', 'woocommerce-gateway-cardknox'),
         ),
     ),
-    $keyPrefix . '_button_style' => array(
-        $keyTitle       => __($buttonPrefix . ' Style', 'woocommerce-gateway-cardknox'),
-        $keyType        => $selectType,
-        $keyOptions     => array(
+    $gkeyPrefix . '_button_style' => array(
+        $gkeyTitle       => __($gbuttonPrefix . ' Style', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => $gselectType,
+        $gkeyOptions     => array(
             'black'          => __('Black', 'woocommerce-gateway-cardknox'),
             'white'          => __('White', 'woocommerce-gateway-cardknox'),
-            //'whiteOutline'   => __('WhiteOutline', 'woocommerce-gateway-cardknox'),
         ),
     ),
-    $keyPrefix . '_capture' => array(
-        $keyTitle       => __($googlePrefix . ' Capture', 'woocommerce-gateway-cardknox'),
+    $gkeyPrefix . '_capture' => array(
+        $gkeyTitle       => __($googlePrefix . ' Capture', 'woocommerce-gateway-cardknox'),
         'label'       => __('Capture charge immediately', 'woocommerce-gateway-cardknox'),
-        $keyType        => 'checkbox',
+        $gkeyType        => 'checkbox',
         'id'          => 'google_cardknox_capture',
         'description' => __(
             'If the transaction is not immediately captured for Google Pay, it will require capturing at a later time.',
             'woocommerce-gateway-cardknox'
         ),
-        $keyDefault     => 'yes',
-        $keyDescTip    => true,
+        $gkeyDefault     => 'yes',
+        $gkeyDescTip    => true,
     ),
-    $keyPrefix . '_auth_only_order_status' => array(
-        $keyTitle       => __('Authorize Only Order Status', 'woocommerce-gateway-cardknox'),
-        $keyType        => $selectType,
+    $gkeyPrefix . '_auth_only_order_status' => array(
+        $gkeyTitle       => __('Authorize Only Order Status', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => $gselectType,
         'class'       => 'wc-enhanced-select',
         'id'          => 'google_cardknox_authonly_status',
         'description' => __(
             'Defines the intended order status after an authorization-only transaction for Google Pay.',
             'woocommerce-gateway-cardknox'
         ),
-        $keyDefault     => 'on-hold',
-        $keyDescTip    => true,
-        $keyOptions     => array(
+        $gkeyDefault     => 'on-hold',
+        $gkeyDescTip    => true,
+        $gkeyOptions     => array(
             'on-hold'        => __(
                 'Set order status to on-hold when payment is authorized',
                 'woocommerce-gateway-cardknox'
@@ -92,18 +91,18 @@ $wc_cardknox_google_pay_settings = array(
             ),
         ),
     ),
-    $keyPrefix . '_applicable_countries' => array(
-        $keyTitle       => __('Payment From Applicable Countries', 'woocommerce-gateway-cardknox'),
-        $keyType        => $selectType,
-        $keyOptions     => array(
+    $gkeyPrefix . '_applicable_countries' => array(
+        $gkeyTitle       => __('Payment From Applicable Countries', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => $gselectType,
+        $gkeyOptions     => array(
             '0'      => __('All Allowed Countries', 'woocommerce-gateway-cardknox'),
             '1'      => __('Specific Countries', 'woocommerce-gateway-cardknox'),
         ),
     ),
-    $keyPrefix . '_specific_countries' => array(
-        $keyTitle       => __('Payment From Specific Countries', 'woocommerce-gateway-cardknox'),
-        $keyType        => 'multiselect',
-        $keyOptions     => $countries
+    $gkeyPrefix . '_specific_countries' => array(
+        $gkeyTitle       => __('Payment From Specific Countries', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => 'multiselect',
+        $gkeyOptions     => $countries
     ),
 );
 

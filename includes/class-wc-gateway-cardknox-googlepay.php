416a698bf9a6f2122e1e95f45cfec79be384ee2b
Pritesh Modi <pritesh.lr@gmail.com>
2024-09-10 20:52:31 +0530

    PLGN-126 Added Quick checkout to WooCommerce cart page (#49)
    * Add google pay button for cart page
    
    * Added GPay button for cart page
    
    * Add Quick checkout Gpay button
    
    * Applepay - Add Quick checkout to WooCommerce cart
    
    * Applepay and Googlepay - Add Quick checkout to WooCommerce cart
    
    * Applepay and Googlepay - Add Quick checkout to WooCommerce cart
    
    * Applepay and Googlepay - Add Quick checkout to WooCommerce cart
    
    * Fixed button size issue
    
    * Fixed sonarcloud issue
    
    * Fixed conflict PLGN-126 Branch
    
    * Fixed conflict PLGN-126 Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch
    
    * Fixed conflict PLGN-126  Branch

--- includes/class-wc-gateway-cardknox-googlepay.php
+++ includes/class-wc-gateway-cardknox-googlepay.php
@@ -50,24 +50,29 @@ class WCCardknoxGooglepay extends WC_Payment_Gateway_CC
         $option = get_option('woocommerce_cardknox_settings');
 
         $this->enabled                          = $this->get_option('googlepay_enabled');
+        $this->google_quickcheckout             = $this->get_option('googlepay_quickcheckout');
         $this->title                            = $this->get_option('googlepay_title');
         $this->description                      = __('Pay with your Google Pay.', 'woocommerce-gateway-cardknox');
         $this->googlepay_merchant_name          = $this->get_option('googlepay_merchant_name');
         $this->googlepay_environment            = $this->get_option('googlepay_environment');
         $this->googlepay_button_style           = $this->get_option('googlepay_button_style');
-        $this->googlepay_button_type            = $this->get_option('googlepay_button_type');
-        $this->capture                          = 'yes' === $option['capture'];
-        $this->authonly_status                  = $option['auth_only_order_status'];
-        $this->googlepay_applicable_countries   = $option['applicable_countries'];
-        $this->googlepay_specific_countries     = $option['specific_countries'];
+        $this->capture                          = 'yes' === $this->get_option('googlepay_capture', 'yes');
+        $this->authonly_status                  = $this->get_option('googlepay_auth_only_order_status');
+        $this->googlepay_applicable_countries   = $this->get_option('googlepay_applicable_countries');
+        $this->googlepay_specific_countries     = $this->get_option('googlepay_specific_countries');
 
         $this->wcVersion = version_compare(WC_VERSION, '3.0.0', '<');
+
         // Hooks.
         add_action('wp_enqueue_scripts', array($this, 'gpayment_scripts'));
         add_action('woocommerce_update_options_payment_gateways_cardknox', array($this, 'process_admin_options'));
 
         add_action('woocommerce_review_order_after_submit', array($this, 'cardknox_gpay_order_after_submit'));
         add_filter('woocommerce_available_payment_gateways', array($this, 'cardknox_allow_gpay_method_by_country'));
+
+        if (is_cart() && $this->google_quickcheckout == 'no') {
+            add_action('woocommerce_proceed_to_checkout', array($this, 'cardknox_gpay_order_after_submit'), 20);
+        }
     }
 
     /**
--- includes/settings-cardknox-googlepay.php
+++ includes/settings-cardknox-googlepay.php
@@ -23,6 +23,16 @@ $wc_cardknox_google_pay_settings = array(
             'no'     => __('No', 'woocommerce-gateway-cardknox'),
         ),
     ),
+    $gkeyPrefix . '_quickcheckout' => array(
+        $gkeyTitle       => __('Disable Quick Checkout to Cart Page', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => 'checkbox',
+        'description' => __(
+            'Disable Quick Checkout to Cart Page',
+            'woocommerce-gateway-cardknox'
+        ),
+        $gkeyDefault    => 'no',
+        $gkeyDescTip    => true,
+    ),
     $gkeyPrefix . '_title' => array(
         $gkeyTitle       => __('Title', 'woocommerce-gateway-cardknox'),
         $gkeyType        => $gtextType,
@@ -42,13 +52,60 @@ $wc_cardknox_google_pay_settings = array(
         ),
     ),
     $gkeyPrefix . '_button_style' => array(
-        $gkeyTitle       => __($gbuttonPrefix . ' Style', 'woocommerce-gateway-cardknox'),
+        $gkeyTitle       => __($gbuttonPrefix . 'Style', 'woocommerce-gateway-cardknox'),
         $gkeyType        => $gselectType,
         $gkeyOptions     => array(
             'black'          => __('Black', 'woocommerce-gateway-cardknox'),
             'white'          => __('White', 'woocommerce-gateway-cardknox'),
         ),
-    )
+    ),
+    $gkeyPrefix . '_capture' => array(
+        $gkeyTitle       => __($googlePrefix . 'Capture', 'woocommerce-gateway-cardknox'),
+        'label'       => __('Capture charge immediately', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => 'checkbox',
+        'id'          => 'google_cardknox_capture',
+        'description' => __(
+            'If the transaction is not immediately captured for Google Pay, it will require capturing at a later time.',
+            'woocommerce-gateway-cardknox'
+        ),
+        $gkeyDefault     => 'yes',
+        $gkeyDescTip    => true,
+    ),
+    $gkeyPrefix . '_auth_only_order_status' => array(
+        $gkeyTitle       => __('Authorize Only Order Status', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => $gselectType,
+        'class'       => 'wc-enhanced-select',
+        'id'          => 'google_cardknox_authonly_status',
+        'description' => __(
+            'Defines the intended order status after an authorization-only transaction for Google Pay.',
+            'woocommerce-gateway-cardknox'
+        ),
+        $gkeyDefault     => 'on-hold',
+        $gkeyDescTip    => true,
+        $gkeyOptions     => array(
+            'on-hold'        => __(
+                'Set order status to on-hold when payment is authorized',
+                'woocommerce-gateway-cardknox'
+            ),
+            'processing' => __(
+                'Set order status to processing when payment is authorized',
+                'woocommerce-gateway-cardknox'
+            ),
+        ),
+    ),
+    $gkeyPrefix . '_applicable_countries' => array(
+        $gkeyTitle       => __('Payment From Applicable Countries', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => $gselectType,
+        $gkeyOptions     => array(
+            '0'      => __('All Allowed Countries', 'woocommerce-gateway-cardknox'),
+            '1'      => __('Specific Countries', 'woocommerce-gateway-cardknox'),
+        ),
+    ),
+    $gkeyPrefix . '_specific_countries' => array(
+        $gkeyTitle       => __('Payment From Specific Countries', 'woocommerce-gateway-cardknox'),
+        $gkeyType        => 'multiselect',
+        $gkeyOptions     => $countries
+    ),
 );
 
 $GLOBALS["wc_cardknox_google_pay_settings"] = $wc_cardknox_google_pay_settings;

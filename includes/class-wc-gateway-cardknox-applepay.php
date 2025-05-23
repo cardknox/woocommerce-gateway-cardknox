d3a4fb8fc37e78161e09886850f3ac369999700d
Pritesh Modi <pritesh.lr@gmail.com>
2025-05-23 05:22:05 -0400

    define the variables and remove space

--- includes/class-wc-gateway-cardknox-applepay.php
+++ includes/class-wc-gateway-cardknox-applepay.php
@@ -18,13 +18,6 @@ class WCCardknoxApplepay extends WC_Payment_Gateway_CC
      */
     public $capture;
     public $applepay_button_style;
-    public $applepay_merchant_identifier;
-    public $applepay_environment;
-	public $applepay_button_type;
-	public $authonly_status;
-	public $applepay_applicable_countries;
-	public $applepay_specific_countries;
-	public $wcVersion;
 
     public function __construct()
     {

31348560df5615dfe3267548789f9bf15dfd3c38
Pritesh Modi <pritesh.lr@gmail.com>
2025-08-07 20:45:55 +0530

    PLGN-135 Build generate

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'bbc277bd4d7655907626');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '5b6d81d9b6b7d4aef145');

5ed87a52856f8a039ea22f024ad059c1ef955ff1
Pritesh Modi <pritesh.lr@gmail.com>
2025-09-15 06:25:48 +0000

    uload build file

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'cf942c7a587477dbfbf6');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '945f560e6a9ab3c79a27');

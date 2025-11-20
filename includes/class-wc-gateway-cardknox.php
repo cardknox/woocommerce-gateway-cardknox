cc93473956bf849f819da0c5965d805dfaf90197
Moshe Stein <47797106+mstein-ck@users.noreply.github.com>
2025-11-20 16:32:59 -0500

    Apply suggestion from @mstein-ck

--- includes/class-wc-gateway-cardknox.php
+++ includes/class-wc-gateway-cardknox.php
@@ -696,7 +696,7 @@ class WC_Gateway_Cardknox extends WC_Payment_Gateway_CC
                     if (is_wp_error($response)) {
                         $order->add_order_note($response->get_error_message());
                         throw new WC_Data_Exception(
-                            __( 'The transaction was declined please try again.', 'woocommerce-gateway-cardknox' )
+                            __( 'The transaction was declined. Please try again.', 'woocommerce-gateway-cardknox' )
                         );
                     }
 

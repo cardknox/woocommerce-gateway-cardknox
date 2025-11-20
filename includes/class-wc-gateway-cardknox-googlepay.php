b697053e49d6dc83257f44a8d8e4f0faa60b924a
Moshe Stein <47797106+mstein-ck@users.noreply.github.com>
2025-11-20 16:32:20 -0500

    Apply suggestion from @mstein-ck

--- includes/class-wc-gateway-cardknox-googlepay.php
+++ includes/class-wc-gateway-cardknox-googlepay.php
@@ -346,7 +346,7 @@ class WCCardknoxGooglepay extends WC_Payment_Gateway_CC
                 if (is_wp_error($response)) {
                     $orderGooglePay->add_order_note($response->get_error_message());
                     throw new WC_Data_Exception(
-                        __( 'The transaction was declined please try again.', 'woocommerce-gateway-cardknox' )
+                        __( 'The transaction was declined. Please try again.', 'woocommerce-gateway-cardknox' )
                     );
                 }
 
